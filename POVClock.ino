bc7911e5d4e66d7f03ac1ecf3749b70addf78c7e
Jeremy Philemon <jeremyphilemon@outlook.com>
2016-11-18 23:17:01 +0530

    Updated the signature.

--- POVClock.ino
+++ POVClock.ino
@@ -7,8 +7,7 @@
 
   This code was debugged using an Arduino Pro Mini 5V 16MHz with the LEDs connected to the pins 4, 5, 6, 7, and 8. Currently, Only manual input of time works.
 
-  created 2 Nov 2014
-  by Jeremy Philemon
+  Created on the 2nd day of November, 2016 by Jeremy Philemon
 */
 
 
